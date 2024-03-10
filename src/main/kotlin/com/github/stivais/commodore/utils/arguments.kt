e849d5c2d8c9dcdef76855c83be6dbbf2f79a1e1
stivais <stivaisss@gmail.com>
2024-03-10 11:14:27 +0200

    pls jitpack work

--- src/main/kotlin/com/github/stivais/commodore/utils/arguments.kt
+++ src/main/kotlin/com/github/stivais/commodore/utils/arguments.kt
@@ -11,6 +11,9 @@ package com.github.stivais.commodore.utils
  *      println("$string") // when given the input of "hello world" it doesn't accept it because too many arguments
  *  }
  * ```
+ *
+ * NOTE: You can't use any more parameters past a greedy string as it takes all remaining strings,
+ * causing the command to error because it is unable to fulfil the remaining parameters
  */
 data class GreedyString(val string: String) {
     override fun toString(): String = string
