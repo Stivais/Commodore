74caeeae3c87d86d203c3f9ba82f140c4439a25a
stivais <stivaisss@gmail.com>
2025-01-19 19:06:43 +0200

    make jitpack use jdk 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,32 +5,41 @@ plugins {
     kotlin("jvm")
     id("gg.essential.multi-version")
     id("gg.essential.defaults")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     `maven-publish`
 }
 
-group = "com.github.stivais"
-version = project.findProperty("version") as String
-
 val isLegacy = project.name == "legacy"
+
+group = "com.github.stivais"
+version = "${project.findProperty("version") as String}${if (isLegacy) "-legacy" else ""}"
+val versionString = version.toString()
+
+loom {
+    noRunConfigs()
+}
 
 repositories {
     mavenCentral()
     maven(url = "https://libraries.minecraft.net")
 }
 
+val embed: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-
-    implementation("com.mojang:brigadier:1.2.9")
+    embed("com.mojang:brigadier:1.2.9")
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            artifactId = if (isLegacy) "Commodore-legacy" else "Commodore"
+            artifactId = "Commodore"
             groupId = project.group as String
-            version = project.findProperty("version") as String
+            version = versionString
             artifact(tasks.jar.get().archiveFile)
         }
     }
@@ -40,7 +49,19 @@ publishing {
 }
 
 java.withSourcesJar()
-loom.noRunConfigs()
+
+tasks {
+    jar {
+        archiveBaseName.set("Commodore")
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        archiveClassifier.set("all")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        configurations = listOf(embed)
+        mergeServiceFiles()
+    }
+}
 
 kotlin {
     compilerOptions {
